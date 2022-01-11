bc176ff856d50cffcad47436e13c7c67e81fae0f
zackzydonik <zackzydonik@gmail.com>
2022-01-10 21:17:50 -0500

    updated Query

--- Queries.sql
+++ Queries.sql
@@ -3,10 +3,10 @@ select * from "Target";
 select * from "Features";
 
 -- Joining Features and Target tables
-SELECT Target.CMPLNT_NUM,
-    Target.PATROL_BORO,
-	Target.CMPLNT_FR_DT,
+SELECT t.cmplnt_num,
+    t.patrol_boro,
+	t.cmplnt_fr_dt
 INTO JoinedTable
-FROM Target
-LEFT JOIN Features
-ON Target.CMPLNT_NUM = Features.CMPLNT_NUM;
+FROM target as t
+LEFT JOIN features as f
+ON t.cmplnt_num = f.cmplnt_num;
